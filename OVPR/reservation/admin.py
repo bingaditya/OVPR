74d6c1b86f1290b502b736e0f5b1c2ae957ffe9b
ADITYA RAJ <56356053+bingaditya@users.noreply.github.com>
2021-04-26 17:29:32 +0530

    Add files via upload

---
+++ OVPR/reservation/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from reservation.models import *
+
+admin.site.register(ParkingPlaces)
+admin.site.register(User)
+admin.site.register(Reservation)
+
+# Register your models here.
